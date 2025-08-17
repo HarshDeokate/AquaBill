93fa31c8c3dd8bc96c92bec33ef09ab49cd857cd
HarshDeokate <harshd041126@gmail.com>
2025-08-17 18:34:36 +0530

    Initial Commit

--- AquaBill/src/components/Navbar.jsx
+++ AquaBill/src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 export default function Navbar() {
   return (
-    <nav className="bg-blue-600 p-4 text-white font-bold">
-      Water Jar Billing
+    <nav className="flex items-center bg-black p-4 text-white font-bold h-20">
+      <img className = "h-15" src="/logo.png" alt="💧" />
+      Harsh Enterprisses
     </nav>
   );
 }
